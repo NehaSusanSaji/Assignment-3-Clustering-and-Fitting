173a80bf119acb05d44ba8bd37caa7deb7b89b3f
NehaSusanSaji <125890665+NehaSusanSaji@users.noreply.github.com>
2023-05-11 14:27:11 +0100

    Add files via upload

--- Assignment-3.py
+++ Assignment-3.py
@@ -12,6 +12,7 @@ import cluster_tools as ct
 import scipy.optimize as opt
 import errors as err
 
+
 def read_data(file):
     """
     The function accepts a file and reads it into a pandas DataFrame and 
@@ -31,7 +32,7 @@ def read_data(file):
         The transposed version of the cleaned DataFrame.
 
     """
-    
+
     # reads in an excel file
     if ".xlsx" in file:
         df = pd.read_excel(file, index_col=0)
@@ -48,8 +49,9 @@ def read_data(file):
     return df_clean, df_t
 
 
-#for reproducibility
+# for reproducibility
 np.random.seed(10)
+
 
 def kmeans_cluster(nclusters):
     """
@@ -71,11 +73,11 @@ def kmeans_cluster(nclusters):
 
     """
     kmeans = cluster.KMeans(n_clusters=nclusters)
-    #df_cluster is the dataframe in which clustering is performed
-    kmeans.fit(df_cluster) 
+    # df_cluster is the dataframe in which clustering is performed
+    kmeans.fit(df_cluster)
     labels = kmeans.labels_
     cen = kmeans.cluster_centers_
-    
+
     return labels, cen
 
 
@@ -100,60 +102,60 @@ def poly(x, a, b, c):
         The polynomial curve.
 
     """
-    
+
     x = x - 2003
     f = a + b*x + c*x**2
-    
+
     return f
 
 
-#the csv files are read into dataframes
+# the csv files are read into dataframes
 _, co2_df = read_data("co2_emissions.csv")
 print(co2_df)
 
 _, gdp_per_capita_df = read_data("gdp_per_capita.csv")
 print(gdp_per_capita_df)
 
-#Specific columns are extracted
+# Specific columns are extracted
 co2_india = co2_df.loc[:, "India"].copy()
 print(co2_india)
 
 gdp_per_capita_india = gdp_per_capita_df.loc["1990":"2019", "India"].copy()
 print(gdp_per_capita_india)
 
-#The extracted columns are merged into a dataframe
-df_india = pd.merge(co2_india, gdp_per_capita_india, on=co2_india.index,\
+# The extracted columns are merged into a dataframe
+df_india = pd.merge(co2_india, gdp_per_capita_india, on=co2_india.index,
                     how="outer")
-df_india = df_india.rename(columns={'key_0':"Year", 'India_x':"co2_emissions",\
-                                    'India_y':"gdp_per_capita"})
+df_india = df_india.rename(columns={'key_0': "Year", 'India_x': "co2_emissions",
+                                    'India_y': "gdp_per_capita"})
 df_india = df_india.set_index("Year")
 print(df_india)
 
-#the scatter matrix of the dataframe is plotted
+# the scatter matrix of the dataframe is plotted
 pd.plotting.scatter_matrix(df_india)
 
-#The dataframe for clustering is created
+# The dataframe for clustering is created
 df_cluster = df_india[["co2_emissions", "gdp_per_capita"]].copy()
 
-#The data is normalized
+# The data is normalized
 df_cluster, df_min, df_max = ct.scaler(df_cluster)
 
-#The number of clusters and respective silhouette scores are printed
+# The number of clusters and respective silhouette scores are printed
 print("n   score")
 for ncluster in range(2, 10):
-    lab, cent = kmeans_cluster(ncluster) 
+    lab, cent = kmeans_cluster(ncluster)
     print(ncluster, skmet.silhouette_score(df_cluster, lab))
-    
-#The cluster centers and labels are calculated using the function
+
+# The cluster centers and labels are calculated using the function
 label, center = kmeans_cluster(5)
 xcen = center[:, 0]
 ycen = center[:, 1]
 
-#The clustering is plotted
+# The clustering is plotted
 plt.figure()
 cm = plt.cm.get_cmap('Set1')
-plt.scatter(df_cluster['gdp_per_capita'], df_cluster["co2_emissions"], s=10,\
-            c=label, marker='o', cmap=cm)    
+plt.scatter(df_cluster['gdp_per_capita'], df_cluster["co2_emissions"], s=10,
+            c=label, marker='o', cmap=cm)
 plt.scatter(xcen, ycen, s=20, c="k", marker="d")
 plt.title("CO2 emission vs GDP per capita of India")
 plt.xlabel("GDP per capita")
@@ -161,17 +163,17 @@ plt.ylabel("CO2 emissions")
 plt.legend()
 plt.show()
 
-#The cluster centres are rescaled to original scale
+# The cluster centres are rescaled to original scale
 centre = ct.backscale(center, df_min, df_max)
 xcen = centre[:, 0]
 ycen = centre[:, 1]
 
 
-#The clustering is plotted with the original scale
+# The clustering is plotted with the original scale
 plt.figure()
 cm = plt.cm.get_cmap('Set1')
 plt.scatter(df_india['gdp_per_capita'], df_india["co2_emissions"], 10,
-            label, marker='o', cmap=cm)    
+            label, marker='o', cmap=cm)
 plt.xlabel("GDP per capita")
 plt.ylabel("CO2 emissions")
 plt.title("CO2 emission vs GDP per capita of India")
@@ -180,7 +182,7 @@ plt.show()
 
 t = ['1990', '1995', '2000', '2005', '2010', '2015', '2020']
 
-#The plot of CO2 Emissions(1990-2019) in India is plotted
+# The plot of CO2 Emissions(1990-2019) in India is plotted
 plt.plot(df_india.index, df_india['co2_emissions'])
 plt.xlabel("Years")
 plt.ylabel("CO2 Emissions (metric tons per capita)")
@@ -188,7 +190,7 @@ plt.title("CO2 Emissions (1990-2019)")
 plt.xticks(ticks=t, labels=t)
 plt.show()
 
-#The plot of GDP per capita (1990-2019) in India is plotted
+# The plot of GDP per capita (1990-2019) in India is plotted
 plt.plot(df_india.index, df_india["gdp_per_capita"])
 plt.xlabel("Years")
 plt.ylabel("GDP per capita")
@@ -196,24 +198,24 @@ plt.title("GDP per capita (1990-2019)")
 plt.xticks(ticks=t, labels=t)
 plt.show()
 
-#The dataframe is prepared for fitting
+# The dataframe is prepared for fitting
 df_india = df_india.reset_index()
 df_india["gdp_per_capita"] = pd.to_numeric(df_india["gdp_per_capita"])
 df_india["Year"] = pd.to_numeric(df_india["Year"])
 
-#The fitting of the GDP per capita plot
-#Calculates the parameters and covariance
-param, covar = opt.curve_fit(poly, df_india["Year"],\
+# The fitting of the GDP per capita plot
+# Calculates the parameters and covariance
+param, covar = opt.curve_fit(poly, df_india["Year"],
                              df_india["gdp_per_capita"])
-#Calculates the standard deviation    
+# Calculates the standard deviation
 sigma = np.sqrt(np.diag(covar))
 year = np.arange(1990, 2030)
-#Calculates the fitting curve
+# Calculates the fitting curve
 forecast = poly(year, *param)
-#Calculates the confidence range
+# Calculates the confidence range
 low, up = err.err_ranges(year, poly, param, sigma)
 df_india["fit1"] = poly(df_india["Year"], *param)
-#Plots the graph with fitting and confidence range
+# Plots the graph with fitting and confidence range
 plt.figure()
 plt.plot(df_india["Year"], df_india["gdp_per_capita"], label="GDP", c='blue')
 plt.plot(year, forecast, label="forecast", c='red')
@@ -224,17 +226,17 @@ plt.title("GDP forecast of India")
 plt.legend()
 plt.show()
 
-#The fitting of CO2 Emissions plot 
-#Calculates the parameters and covariance
+# The fitting of CO2 Emissions plot
+# Calculates the parameters and covariance
 param, covar = opt.curve_fit(poly, df_india["Year"], df_india["co2_emissions"])
-#Calculates the standard deviation
+# Calculates the standard deviation
 sigma = np.sqrt(np.diag(covar))
-#Calculates the fitting curve
+# Calculates the fitting curve
 forecast = poly(year, *param)
-#Calculates the confidence range
+# Calculates the confidence range
 low, up = err.err_ranges(year, poly, param, sigma)
 df_india["fit2"] = poly(df_india["Year"], *param)
-#Plots the graph with fitting and confidence range
+# Plots the graph with fitting and confidence range
 plt.figure()
 plt.plot(df_india["Year"], df_india["co2_emissions"], label="CO2 emissions",
          c='green')
@@ -245,18 +247,3 @@ plt.ylabel("CO2 Emissions (metric tons per capita)")
 plt.title("CO2 Emissions Forecast of India")
 plt.legend()
 plt.show()
-
-                            
-
-
-
-
-
-
-
-
-
-
-
-
-
